d1753feb0524e57a9e1382b295ef8cf4f7cccfc5
warunsinx <warunsinx@gmail.com>
2024-02-15 02:09:48 +0700

    ci: :wrench: add docker file & docker-compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import configuration from './config/configuration';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.env.development.local',
+      envFilePath: '.env',
       load: [configuration],
       isGlobal: true,
     }),
