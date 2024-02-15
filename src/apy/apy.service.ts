d18f5c1646685646f11e844992bb01cb080ca8ab
warunsinx <warunsinx@gmail.com>
2024-02-16 01:58:20 +0700

    feat: :sparkles: update apy service

--- src/apy/apy.service.ts
+++ src/apy/apy.service.ts
@@ -1,13 +1,73 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { GetPoolDto } from '../pool/dtos/getPool.dto';
 import { PoolService } from 'src/pool/pool.service';
+import { request, gql } from 'graphql-request';
+import configuration from 'src/config/configuration';
+import { ethers } from 'ethers';
+import { calculateAPY } from 'src/utils/calculateAPY';
 
 @Injectable()
 export class ApyService {
   constructor(private readonly poolService: PoolService) {}
 
-  getApy(getPoolDto: GetPoolDto) {
+  async getApy(getPoolDto: GetPoolDto) {
     const pool = this.poolService.getPool(getPoolDto);
-    return pool;
+
+    if (!pool) {
+      throw new BadRequestException('Pool not found');
+    }
+
+    const provider = new ethers.JsonRpcProvider(
+      configuration().chain.ethereum.rpc_url,
+    );
+
+    const blockNumber = await provider.getBlockNumber();
+
+    const last24hrsBlock =
+      +blockNumber - +configuration().chain.ethereum.block_time * 60 * 60 * 24;
+
+    const document = gql`
+      query QueryPoolData {
+        pool(
+          id: "${getPoolDto.poolAddress}"
+          block: { number_gte: ${last24hrsBlock} }
+        ) {
+          totalValueLockedToken0,
+          totalValueLockedToken1
+          feesUSD,
+          feeTier,
+          volumeToken0
+          volumeToken1
+        }
+      }
+    `;
+
+    const res = (await request(
+      configuration().subgrpah.uniswap_v3_endpoint,
+      document,
+    )) as unknown as {
+      pool: {
+        totalValueLockedToken0: string;
+        totalValueLockedToken1: string;
+        feesUSD: string;
+        feeTier: string;
+        volumeToken0: string;
+        volumeToken1: string;
+      };
+    };
+
+    const poolData = res.pool;
+
+    return calculateAPY(
+      {
+        feeTier: poolData.feeTier,
+        volumeToken0: poolData.volumeToken0,
+        volumeToken1: poolData.volumeToken1,
+      },
+      {
+        totalValueLockedToken0: +poolData.totalValueLockedToken0,
+        totalValueLockedToken1: +poolData.totalValueLockedToken1,
+      },
+    );
   }
 }
---
+++ src/utils/calculateAPY.ts
@@ -0,0 +1,28 @@
+interface PoolData {
+  feeTier: string;
+  volumeToken0: string;
+  volumeToken1: string;
+}
+
+interface HistoricalData {
+  totalValueLockedToken0: number;
+  totalValueLockedToken1: number;
+}
+
+export function calculateAPY(
+  poolData: PoolData,
+  historicalData: HistoricalData,
+): number {
+  console.log(poolData, historicalData);
+  const feePercentage: number = parseFloat(poolData.feeTier) / 10000;
+  const feesToken0: number = parseFloat(poolData.volumeToken0) * feePercentage;
+  const feesToken1: number = parseFloat(poolData.volumeToken1) * feePercentage;
+  const totalFees: number = feesToken0 + feesToken1;
+
+  const totalValueLocked: number =
+    historicalData.totalValueLockedToken0 +
+    historicalData.totalValueLockedToken1;
+
+  const apy: number = Math.pow(1 + totalFees / totalValueLocked, 365) - 1;
+  return apy * 100;
+}
